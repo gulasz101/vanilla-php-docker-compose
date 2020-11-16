6c5cd8c5301641dad0cafbf0a554ea2cdbf28279
wojciechgula <gulasz101@gmail.com>
2020-11-17 00:56:26 +0100

    Setting up phpunit.

---
+++ src/tests/Unit/PhpInfoHasMethodRunTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\PhpInfo;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PhpInfoHasMethodRunTest
+ * @package Tests\Unit
+ */
+class PhpInfoHasMethodRunTest extends TestCase
+{
+    public function testPositive(): void
+    {
+        $this->assertTrue(method_exists(new PhpInfo(), 'run'));
+    }
+}
